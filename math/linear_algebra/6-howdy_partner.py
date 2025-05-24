80765b7831e655c693f6dd88840a5669f31909d4
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2025-05-24 15:53:23 +0200

    Update 6-howdy_partner.py

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
+
+"""
+Module for concatenating two 1D arrays.
+"""
+
+
 def cat_arrays(arr1, arr2):
+    """
+    Concatenates two 1D arrays.
+
+    Args:
+        arr1 (list): First array.
+        arr2 (list): Second array.
+
+    Returns:
+        list: New array containing elements of both inputs.
+    """
     return arr1 + arr2
