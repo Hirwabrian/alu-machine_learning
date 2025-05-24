ded3bfb0ba52c65980adf7082bd97148bd22f579
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2025-05-24 16:12:59 +0200

    Update 8-ridin_bareback.py

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -14,7 +14,8 @@ def mat_mul(mat1, mat2):
         mat2 (list of lists): The second matrix.
 
     Returns:
-        list of lists or None: The result of mat1 × mat2, or None if shapes are incompatible.
+        list of lists or None: The result of mat1 × mat2, 
+        or None if shapes are incompatible.
     """
     if len(mat1[0]) != len(mat2):
         return None
