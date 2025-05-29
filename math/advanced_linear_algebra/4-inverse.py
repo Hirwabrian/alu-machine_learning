9ce0d2459f2a414f3dfa86ab634d0019d3a7a279
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2025-05-29 10:24:51 +0200

    Update 4-inverse.py

--- math/advanced_linear_algebra/4-inverse.py
+++ math/advanced_linear_algebra/4-inverse.py
@@ -1,11 +1,28 @@
+#!/usr/bin/env python3
+
 """
 Calculates the inverse of a matrix.
 """
+
+
 import importlib
 det_mod = importlib.import_module("0-determinant")
 adj_mod = importlib.import_module("3-adjugate")
 
 def inverse(matrix):
+    """
+    Calculates the inverse of a matrix using adjugate and determinant.
+
+    Args:
+        matrix (list of lists): The input matrix.
+
+    Returns:
+        list of lists: Inverse of the matrix or None if singular.
+
+    Raises:
+        TypeError: If matrix is not a list of lists.
+        ValueError: If matrix is not a non-empty square matrix.
+    """
     if not isinstance(matrix, list) or matrix == [] or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
     if len(matrix) == 0 or any(len(row) != len(matrix) for row in matrix):
