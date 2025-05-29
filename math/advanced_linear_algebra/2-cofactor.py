31bc276ee2b04e1db1006d8b436023660f18d11f
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2025-05-29 10:22:44 +0200

    Update 2-cofactor.py

--- math/advanced_linear_algebra/2-cofactor.py
+++ math/advanced_linear_algebra/2-cofactor.py
@@ -1,11 +1,27 @@
+#!/usr/bin/env python3
+
 """
 Calculates the cofactor matrix of a matrix.
 """
-import importlib
 
+
+import importlib
 minr = importlib.import_module("1-minor")
 
 def cofactor(matrix):
+    """
+    Computes the cofactor matrix from a given matrix.
+
+    Args:
+        matrix (list of lists): The input matrix.
+
+    Returns:
+        list of lists: The cofactor matrix.
+
+    Raises:
+        TypeError: If matrix is not a list of lists.
+        ValueError: If matrix is not a non-empty square matrix.
+    """
     minors = minr.minor(matrix)
     for i in range(len(minors)):
         for j in range(len(minors)):
