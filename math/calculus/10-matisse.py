c12fd09abb4658e9719f098fc0c7dee9f267fa1e
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2025-06-12 20:13:18 +0200

    Update 10-matisse.py

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,13 +1,26 @@
+#!/usr/bin/env python3
+
 """
 Returns the derivative of a polynomial represented by a list.
 """
 
-def poly_derivative(poly):
-    if not isinstance(poly, list) or len(poly) == 0:
-        return None
-    if not all(isinstance(c, (int, float)) for c in poly):
-        return None
-    if len(poly) == 1:
-        return [0]
 
-    return [poly[i] * i for i in range(1, len(poly))]
+def poly_derivative(poly):
+    """
+    calculates the derivative of the given polynomial
+    """
+    if type(poly) is not list or len(poly) < 1:
+        return None
+    for coefficient in poly:
+        if type(coefficient) is not int and type(coefficient) is not float:
+            return None
+    for power, coefficient in enumerate(poly):
+        if power is 0:
+            derivative = [0]
+            continue
+        if power is 1:
+            derivative = []
+        derivative.append(power * coefficient)
+    while derivative[-1] is 0 and len(derivative) > 1:
+        derivative = derivative[:-1]
+    return derivative
