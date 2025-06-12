63d257039987cd921d72da0760ea68429e9f8023
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2025-06-12 20:02:32 +0200

    Update 9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python3
+
 """
 Calculates the sum of squares up to n using a closed formula.
 """
 
+
 def summation_i_squared(n):
-    if not isinstance(n, int) or n < 1:
+    """
+    calculates summation of i^2 from i=1 to n
+    """
+    if type(n) is not int or n < 1:
         return None
-    return n * (n + 1) * (2 * n + 1) // 6
+    sigma_sum = (n * (n + 1) * ((2 * n) + 1)) / 6
+    return int(sigma_sum)
